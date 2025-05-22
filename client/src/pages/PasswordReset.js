e6089e6e4802e4f310bf78c488f5b8f95593998c
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2025-05-22 01:27:11 +0100

    ux ui

--- client/src/pages/PasswordReset.js
+++ client/src/pages/PasswordReset.js
@@ -27,11 +27,11 @@ const PasswordReset = () => {
                  <h2>Forgot Password?</h2>
                  <div className="password-instructions">
                    <p>
-                     Enter the email address you used when you joined and we’ll send you instructions to reset your password.
+                     Please enter the email address associated with your account. We'll send you a link to reset your password and regain access.
                    </p>
-                    <p>
-                    For security reasons, we do NOT store your password. So rest assured that we will never send your password via email.
-                    </p>
+                   <p>
+                     For your security, we never store or send your password by email. If you need further assistance, please contact our support team.
+                   </p>
                  </div>
            
 
